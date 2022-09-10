4df6fb7e7a26a154638ce0771eef57ae7cb4f2c5
RadRussianRus <radrussianrus@gmail.com>
2022-09-10 19:56:41 +0300

    [Option][GUI] "View Profile" on top

--- Telegram/SourceFiles/window/window_peer_menu.cpp
+++ Telegram/SourceFiles/window/window_peer_menu.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "window/window_peer_menu.h"
 
+#include "kotato/kotato_settings.h"
 #include "menu/menu_check_item.h"
 #include "boxes/share_box.h"
 #include "chat_helpers/compose/compose_show.h"
@@ -1289,7 +1290,6 @@ void Filler::fillChatsListActions() {
 		return;
 	}
 	addCreateTopic();
-	addInfo();
 	addViewAsMessages();
 	const auto &all = _peer->forum()->topicsList()->indexed()->all();
 	if (all.size() > kTopicsSearchMinCount) {
@@ -1331,11 +1331,16 @@ void Filler::addVideoChat() {
 }
 
 void Filler::fillContextMenuActions() {
+	const auto profileEnabled = ViewProfileInChatsListContextMenu.value();
+	const auto profileOnTop = ::Kotato::JsonSettings::GetBool("view_profile_on_top");
 	addNewWindow();
 	addHidePromotion();
+	if (profileEnabled && profileOnTop) {
+		addInfo();
+	}
 	addToggleArchive();
 	addTogglePin();
-	if (ViewProfileInChatsListContextMenu.value()) {
+	if (profileEnabled && !profileOnTop) {
 		addInfo();
 	}
 	addToggleMuteSubmenu(false);
