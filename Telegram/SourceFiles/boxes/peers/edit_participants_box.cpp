kotatogram/kotatogram-desktop

[Improvement] Allow to search messages from user

Telegram/SourceFiles/boxes/peers/edit_participants_box.cpp
@@ -7,6 +7,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "boxes/peers/edit_participants_box.h"
 
+#include "kotato/kotato_lang.h"
 #include "api/api_chat_participants.h"
 #include "boxes/peer_list_controllers.h"
 #include "boxes/peers/edit_participant_box.h"
@@ -17,8 +18,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "boxes/add_contact_box.h"
 #include "main/main_session.h"
 #include "mtproto/mtproto_config.h"
+#include "facades.h"
 #include "apiwrap.h"
 #include "lang/lang_keys.h"
+#include "mainwindow.h"
 #include "mainwidget.h"
 #include "dialogs/dialogs_indexed_list.h"
 #include "data/data_peer_values.h"
@@ -1537,6 +1540,20 @@ base::unique_qptr<Ui::PopupMenu> ParticipantsBoxController::rowContextMenu(
 			(participant->isUser()
 				? &st::menuIconProfile
 				: &st::menuIconInfo));
+	}
+	if (const auto window = App::wnd()) {
+		if (const auto mainwidget = window->sessionContent()) {
+			result->addAction(
+				ktr("ktg_context_show_messages_from"),
+				crl::guard(this, [=] {
+					mainwidget->searchMessages(
+						" ",
+						(_peer && !_peer->isUser())
+							? _peer->owner().history(_peer).get()
+							: Dialogs::Key(),
+							user);
+				}), &st::menuIconSearch);
+		}
 	}
 	if (_role == Role::Kicked) {
 		if (_peer->isMegagroup()

kotatogram/kotatogram-desktop-new

Telegram/SourceFiles/boxes/peers/edit_participants_box.cpp
@@ -7,6 +7,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 */
 #include "boxes/peers/edit_participants_box.h"
 
+#include "kotato/kotato_lang.h"
+#include "core/application.h"
 #include "api/api_chat_participants.h"
 #include "boxes/peers/edit_participant_box.h"
 #include "boxes/peers/add_participants_box.h"
@@ -20,6 +22,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "mtproto/mtproto_config.h"
 #include "apiwrap.h"
 #include "lang/lang_keys.h"
+#include "mainwindow.h"
+#include "mainwidget.h"
 #include "dialogs/dialogs_indexed_list.h"
 #include "data/data_peer_values.h"
 #include "data/data_session.h"
@@ -32,6 +36,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "ui/effects/outline_segments.h"
 #include "ui/widgets/popup_menu.h"
 #include "info/profile/info_profile_values.h"
+#include "window/window_controller.h"
 #include "window/window_session_controller.h"
 #include "history/history.h"
 #include "styles/style_menu_icons.h"
@@ -1629,6 +1634,20 @@ base::unique_qptr<Ui::PopupMenu> ParticipantsBoxController::rowContextMenu(
 			(participant->isUser()
 				? &st::menuIconProfile
 				: &st::menuIconInfo));
+	}
+	if (const auto window = _navigation->parentController()) {
+		if (const auto mainwidget = window->widget()->sessionContent()) {
+			result->addAction(
+				ktr("ktg_context_show_messages_from"),
+				crl::guard(this, [=] {
+					mainwidget->searchMessages(
+						" ",
+						(_peer && !_peer->isUser())
+							? _peer->owner().history(_peer).get()
+							: Dialogs::Key(),
+							user);
+				}), &st::menuIconSearch);
+		}
 	}
 	if (const auto by = _additional.restrictedBy(participant)) {
 		result->addAction(