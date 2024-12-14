kotatogram/kotatogram-desktop

[Option][GUI] Emoji panel options

Telegram/SourceFiles/history/view/controls/history_view_compose_controls.cpp
@@ -1563,9 +1563,22 @@ void ComposeControls::initTabbedSelector() {
 		setTabbedPanel(nullptr);
 	}
 
+	base::install_event_filter(_tabbedSelectorToggle, [=](not_null<QEvent*> e) {
+		if (e->type() == QEvent::ContextMenu) {
+			if (::Kotato::JsonSettings::GetBool("emoji_sidebar_right_click")) {
+				toggleTabbedSelectorMode();
+			} else if (_tabbedPanel) {
+				_tabbedPanel->toggleAnimated();
+			}
+			return base::EventFilterResult::Cancel;
+		}
+		return base::EventFilterResult::Continue;
+	});
+
 	_tabbedSelectorToggle->addClickHandler([=] {
-		if (_tabbedPanel && _tabbedPanel->isHidden()) {
-			_tabbedPanel->showAnimated();
+		if (_tabbedPanel && (_tabbedPanel->isHidden()
+				|| ::Kotato::JsonSettings::GetBool("emoji_sidebar_right_click"))) {
+			_tabbedPanel->toggleAnimated();
 		} else {
 			toggleTabbedSelectorMode();
 		}
@@ -2052,7 +2065,8 @@ void ComposeControls::toggleTabbedSelectorMode() {
 	}
 	if (_tabbedPanel) {
 		if (_window->canShowThirdSection()
-				&& !_window->adaptive().isOneColumn()) {
+				&& !_window->adaptive().isOneColumn()
+				&& ::Kotato::JsonSettings::GetBool("emoji_sidebar")) {
 			Core::App().settings().setTabbedSelectorSectionEnabled(true);
 			Core::App().saveSettingsDelayed();
 			pushTabbedSelectorToThirdSection(

kotatogram/kotatogram-desktop-new

Telegram/SourceFiles/history/view/controls/history_view_compose_controls.cpp
@@ -2111,9 +2111,22 @@ void ComposeControls::initTabbedSelector() {
 		setTabbedPanel(nullptr);
 	}
 
+	base::install_event_filter(_tabbedSelectorToggle, [=](not_null<QEvent*> e) {
+		if (e->type() == QEvent::ContextMenu) {
+			if (::Kotato::JsonSettings::GetBool("emoji_sidebar_right_click")) {
+				toggleTabbedSelectorMode();
+			} else if (_tabbedPanel) {
+				_tabbedPanel->toggleAnimated();
+			}
+			return base::EventFilterResult::Cancel;
+		}
+		return base::EventFilterResult::Continue;
+	});
+
 	_tabbedSelectorToggle->addClickHandler([=] {
-		if (_tabbedPanel && _tabbedPanel->isHidden()) {
-			_tabbedPanel->showAnimated();
+		if (_tabbedPanel && (_tabbedPanel->isHidden()
+				|| ::Kotato::JsonSettings::GetBool("emoji_sidebar_right_click"))) {
+			_tabbedPanel->toggleAnimated();
 		} else {
 			toggleTabbedSelectorMode();
 		}
@@ -2853,7 +2866,8 @@ void ComposeControls::toggleTabbedSelectorMode() {
 	}
 	if (_tabbedPanel) {
 		if (_regularWindow->canShowThirdSection()
-				&& !_regularWindow->adaptive().isOneColumn()) {
+				&& !_regularWindow->adaptive().isOneColumn()
+				&& ::Kotato::JsonSettings::GetBool("emoji_sidebar")) {
 			Core::App().settings().setTabbedSelectorSectionEnabled(true);
 			Core::App().saveSettingsDelayed();
 			const auto topic = _history->peer->forumTopicFor(_topicRootId);