kotatogram/kotatogram-desktop

[Visual] Archive icon for collapsed mode

Telegram/SourceFiles/dialogs/ui/dialogs_layout.cpp
@@ -1340,7 +1340,7 @@ void PaintCollapsedRow(
 	const auto narrow = (fullWidth <= smallWidth);
 
 	const auto unreadTop = (st::dialogsImportantBarHeight - st::dialogsUnreadHeight) / 2;
-	if (!narrow || !folder) {
+	if (!folder) {
 		p.setFont(st::semiboldFont);
 		p.setPen(st::dialogsNameFg);
 
@@ -1351,6 +1351,26 @@ void PaintCollapsedRow(
 			? ((fullWidth - st::semiboldFont->width(text)) / 2)
 			: st::dialogsPadding.x();
 		p.drawText(left, textBaseline, text);
+	} else if (!narrow) {
+
+		const auto textBaseline = unreadTop
+			+ (st::dialogsUnreadHeight - st::dialogsUnreadFont->height) / 2
+			+ st::dialogsUnreadFont->ascent;
+		const auto left = st::dialogsPadding.x();
+		const auto textLeft = left * 2 + st::dialogsUnreadHeight;
+
+		folder->paintUserpicLeft(
+			p,
+			row.userpicView(),
+			left,
+			unreadTop,
+			fullWidth,
+			st::dialogsUnreadHeight);
+
+		p.setFont(st::semiboldFont);
+		p.setPen(st::dialogsNameFg);
+		p.drawText(textLeft, textBaseline, text);
+
 	} else {
 		folder->paintUserpicLeft(
 			p,

kotatogram/kotatogram-desktop-new

Telegram/SourceFiles/dialogs/ui/dialogs_layout.cpp
@@ -1003,7 +1003,7 @@ void PaintCollapsedRow(
 	row.paintRipple(p, 0, 0, context.width);
 
 	const auto unreadTop = (st::dialogsImportantBarHeight - st::dialogsUnreadHeight) / 2;
-	if (!context.narrow || !folder) {
+	if (!folder) {
 		p.setFont(st::semiboldFont);
 		p.setPen(st::dialogsNameFg);
 
@@ -1014,6 +1014,24 @@ void PaintCollapsedRow(
 			? ((context.width - st::semiboldFont->width(text)) / 2)
 			: context.st->padding.left();
 		p.drawText(left, textBaseline, text);
+	} else if (!context.narrow) {
+
+		const auto textBaseline = unreadTop
+			+ (st::dialogsUnreadHeight - st::dialogsUnreadFont->height) / 2
+			+ st::dialogsUnreadFont->ascent;
+		const auto left = context.st->padding.left();
+		const auto textLeft = left * 2 + st::dialogsUnreadHeight;
+
+		folder->paintUserpic(
+			p,
+			left,
+			unreadTop,
+			st::dialogsUnreadHeight);
+
+		p.setFont(st::semiboldFont);
+		p.setPen(st::dialogsNameFg);
+		p.drawText(textLeft, textBaseline, text);
+
 	} else {
 		folder->paintUserpic(
 			p,